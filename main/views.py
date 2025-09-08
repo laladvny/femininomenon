97d5300ae7e119d3108f9f75ea9bb1f3f32c4483
laladvny <adzradevany@gmail.com>
2025-09-08 16:04:59 +0700

    Tugas Individu 2

--- main/views.py
+++ main/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name' : 'Yoga Mat',
+        'name': 'Yoga Mat',
         'price': 'IDR 100k',
         'desc': 'Pink',
-        'cat' : 'yoga'
+        'cat': 'yoga'
     }
 
     return render(request, "main.html", context)
