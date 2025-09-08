09fc7ec52699e7c882eaadf40df5a283032e6ae3
laladvny <adzradevany@gmail.com>
2025-09-08 16:07:28 +0700

    Tugas Individu 2

--- femininomenon_sports/settings.py
+++ femininomenon_sports/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "adzradevany-aqiila41/FemininomenonSports"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "adzradevany-aqiila41/femininomenonsports"]
 
 
 # Application definition
